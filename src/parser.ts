aff30e10f4026f3bfbf6d8f1b82f33963fa7efa5
Oxana <lereenadem@yandex.ru>
2021-04-24 15:32:45 +0300

    Use grammar as a dependency

--- src/parser.ts
+++ src/parser.ts
@@ -4,7 +4,7 @@ export async function initializeParser(): Promise<Parser> {
     await Parser.init()
     const parser = new Parser()
 
-    const lang = await Parser.Language.load(`${__dirname}/../tree-sitter-pascalabcnet.wasm`)
+    const lang = await Parser.Language.load(`${__dirname}/../node_modules/tree-sitter-pascalabcnet/tree-sitter-pascalabcnet.wasm`)
 
     parser.setLanguage(lang)
     return parser
